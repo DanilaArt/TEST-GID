5331814e90e08dd845235f57268b56d07e4ffbbc
Danila <aa9527615548@gmail.com>
2026-01-29 17:13:29 +0300

    connect commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+string filePath = "test C.txt";
+
+if (!File.Exists(filePath))
+{
+    Console.WriteLine("Файл не найден");
+    return;
+}
+try
+{
+    using (StreamReader reader = new StreamReader(filePath))
+    {
+        Console.WriteLine("Начинаем чтение файла");
+        string line;
+        while ((line = reader.ReadLine()) != null)
+        {
+            Console.WriteLine(line);
+        }
+    }
+}
+catch(Exception ex)
+{
+    Console.WriteLine($"Произошла ошибка в чтение файла {ex.Message}");
+}
